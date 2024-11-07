a829c47971473dc2a0eacb4bbdbfd6c955a94b94
Bula Rahman <dodoalive1994@gmail.com>
2024-11-07 22:25:43 +0500

    react added 47

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { calc } from "./test";
-
-console.log("init !");
-
-const result = calc(5, 10);
-
-console.log(result);
--- webpack.config.ts
+++ webpack.config.ts
@@ -15,7 +15,7 @@ export default (env: EnvironmentRecord) => {
     // настройка переменной окружения. В дев режиме бандл будет читабельный, в проде - максимально сжатый
     mode: env.mode ?? "development",
     // точка входа, откуда вебпак будет брать исходный код
-    entry: path.resolve(__dirname, "src", "index.ts"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     // точка выхода куда вебпак будет все выгружать финальную сборку
     output: {
       path: path.resolve(__dirname, "dist"),
@@ -34,7 +34,7 @@ export default (env: EnvironmentRecord) => {
     ],
     // Настройка лоудеров. Нужно чтобы вебпак работал с расширениями помимо JS
     module: {
-      // настройка TS
+      // настройка TS. Это loader-TS умеет обрабатывать так же и JSX. Но если бы писали без TS - то пришлось бы ставить babel-loader
       rules: [
         {
           test: /\.tsx?$/,
